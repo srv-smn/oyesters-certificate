794ede7adda601c86026562a6d68a8b852e82561
sourav suman <saurav2fly@gmail.com>
2020-06-18 12:36:45 +0530

    testing for mestamask error

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -15,3 +15,22 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined'){
 }
 
 export default web3 ;
+----------------------------------------------------------------
+import Web3 from 'web3';
+let web3;
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  // We are in the browser and metamask is running.
+//Note: change to window.web3.currentProvider.enable()
+  web3 = new Web3(window.web3.currentProvider.enable());
+} else {
+  // We are on the server *OR* the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/v3/91ae8105018647e7a5302040d40ede18'
+  );
+  web3 = new Web3(provider);
+  //window.web3.currentProvider.enable();
+
+
+}
+
+export default web3;
