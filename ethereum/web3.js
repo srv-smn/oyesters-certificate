1c6dd5fe625864bd1a6e3f3293d25a3771bd988a
sourav suman <saurav2fly@gmail.com>
2020-06-18 13:04:01 +0530

    testing for mestamask error try 3

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -4,6 +4,7 @@ let web3 ;
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined'){
 // we are in browser and running metamask
  web3 = new Web3(window.web3.currentProvider);
+ ethereum.enable();
   }
   else
 {
