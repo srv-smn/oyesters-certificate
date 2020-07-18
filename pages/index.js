cbb351e122b9608112172ed92675246f98500ca6
sourav suman <saurav2fly@gmail.com>
2020-07-18 17:19:47 +0530

    added cert

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,8 @@ class RequestNew extends Component {
 	 name:'',
 	 course:'',
 	 loading : false,
-   errorMessage: ''
+   errorMessage: '',
+   show:false
  };
 
   onSubmit = async event => {
@@ -21,7 +22,8 @@ class RequestNew extends Component {
       const accounts = await web3.eth.getAccounts();
 			const summary = await factory.methods.getData(uid).call();
 			this.setState({name: summary[1]});
-			this.setState({course: summary[2]})
+			this.setState({course: summary[2]});
+      this.setState({show:true});
 
     } catch(err) {
       this.setState({errorMessage:err.message});
@@ -30,6 +32,56 @@ class RequestNew extends Component {
   }
 
   render(){
+
+    let person = null;
+    if(this.state.show)
+    {
+      person = (
+        <div>
+        <style jsx>{
+          `
+          .a11{
+            width:800px;
+            height:600px;
+            padding:20px;
+             text-align:center;
+             border: 10px solid #787878;
+          }
+          .a12{
+            width:750px;
+             height:550px;
+              padding:20px;
+              text-align:center;
+              border: 5px solid #787878;
+          }
+
+          .a13{
+            font-size:50px;
+            font-weight:bold;
+          }
+          .a25{
+            font-size:25px;
+          }
+          .a30{
+            font-size:30px;
+          }
+
+          `
+        }</style>
+        <div className= "a11">
+        <div className= "a12">
+               <p><span className="a13">Certificate of Completion</span></p>
+
+               <span className="a30"><i>This is to certify that </i></span>
+               <span className= "a30" ><b>{this.state.name} </b></span>
+               <span className= "a25" ><i>has completed the course </i></span>
+               <span className= "a30" >{this.state.course} </span>
+               <span className= "a25" >having Id. {this.state.uid} </span>
+        </div>
+        </div>
+        </div>
+      );
+    }
     return(
       <div>
       <h3>Show Certificate</h3>
@@ -42,12 +94,9 @@ class RequestNew extends Component {
             />
           </Form.Field>
 
-          <Message error header="Oops!" content={this.state.errorMessage} />
-					<Message error header="uid!" content={this.state.uid} />
-					<Message error header="name" content={this.state.name} />
-					<Message error header="course" content={this.state.course} />
           <Button primary loading ={this.state.loading}> Show!</ Button>
         </Form>
+        {person}
         </div>
     );
   }
