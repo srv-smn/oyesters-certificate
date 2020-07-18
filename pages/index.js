d0440a2ebe2772db007ad23ee5dc31e5b90f1ceb
sourav suman <saurav2fly@gmail.com>
2020-07-18 21:00:56 +0530

    added certificate with background

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ class RequestNew extends Component {
 	 course:'',
 	 loading : false,
    errorMessage: '',
-   show:false
+   show:true
  };
 
   onSubmit = async event => {
@@ -40,42 +40,62 @@ class RequestNew extends Component {
         <div>
         <style jsx>{
           `
+
           .a11{
             width:800px;
             height:600px;
             padding:20px;
              text-align:center;
-             border: 10px solid #787878;
+            /* border: 8px solid #a87207;*/
+
+             background-image: url("/my-image1.png");
+              background-color: #cccccc;
+              background-position: center;
+              background-repeat: no-repeat;
+              background-size: cover;
+
           }
           .a12{
             width:750px;
              height:550px;
               padding:20px;
               text-align:center;
-              border: 5px solid #787878;
+            /*  border: 5px solid #a87207; */
           }
 
           .a13{
-            font-size:50px;
+            font-size:40px;
             font-weight:bold;
+            color: #a81c07;
           }
           .a25{
             font-size:25px;
           }
           .a30{
+            font-size:25px;
+          }
+          .a31{
             font-size:30px;
+            font-weight:bold;
+            color: #8b15c2;
+            text-decoration: underline;
+          }
+          img {
+            padding: 5px;
+            width: 500px;
+            height: 200;
           }
 
           `
-        }</style>
+        } </style>
         <div className= "a11">
         <div className= "a12">
-               <p><span className="a13">Certificate of Completion</span></p>
-
-               <span className="a30"><i>This is to certify that </i></span>
-               <span className= "a30" ><b>{this.state.name} </b></span>
-               <span className= "a25" ><i>has completed the course </i></span>
-               <span className= "a30" >{this.state.course} </span>
+              <img src="/my-image.png" alt="my image" />
+               <p><span className="a13">Certificate of Course Completion</span></p>
+               <p><span className="a30"><i>This is to certify that </i></span></p>
+               <span className= "a31" ><b>{this.state.name} </b></span>
+              <p> <span className= "a25" ><i>has successfully completed the 7-days hands on webinar on </i></span></p>
+               <p><span className= "a31" >{this.state.course} </span></p>
                <span className= "a25" >having Id. {this.state.uid} </span>
         </div>
         </div>
@@ -83,7 +103,8 @@ class RequestNew extends Component {
       );
     }
     return(
-      <div>
+      
+      <div className ="m2">
       <h3>Show Certificate</h3>
         <Form onSubmit = {this.onSubmit}>
           <Form.Field>
@@ -91,10 +112,11 @@ class RequestNew extends Component {
             <Input
               value = {this.state.uid}
               onChange= {event => this.setState({uid: event.target.value})}
+              placeholder="enter UID"
             />
           </Form.Field>
 
-          <Button primary loading ={this.state.loading}> Show!</ Button>
+          <Button primary loading ={this.state.loading}> Search!</ Button>
         </Form>
         {person}
         </div>
