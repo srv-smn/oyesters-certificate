3356aa5985e2093cd5b91f1bbb7dad21788ed458
sourav suman <saurav2fly@gmail.com>
2020-08-12 22:15:45 +0530

    auto show

--- ethereum/contracts/Certificate.sol
+++ ethereum/contracts/Certificate.sol
@@ -6,51 +6,54 @@ contract Certificate {
         string id ;
         string sname ;
         string course ;
-        // data to be stored in certificate 
+        string imghash ;
+        // data to be stored in certificate
     }
-    
+
     uint total;
     address public manager;
-    mapping(string => Data) certData ;  // mapping so that data can be retrived by id 
-    
+    mapping(string => Data) certData ;  // mapping so that data can be retrived by id
+
     modifier restricted() {
         require(msg.sender==manager);
         _;
     }
-    
+
     function Certificate() public {
         manager = msg.sender;
     }
-    
-     function createData( string _id  , string _sname , string _course) public restricted {
+
+     function createData( string _id  , string _sname , string _course, string _imghash) public restricted {
         Data memory newData = Data({
             id : _id,
             sname : _sname,
-            course : _course
-            });  // creating certificate data 
+            course : _course,
+            imghash: _imghash
+            });  // creating certificate data
         total++;
         certData[_id] = newData;
-    
+
     }
-    
-    function getData(string _id) public view returns (string , string , string) {
-        
+
+    function getData(string _id) public view returns (string , string , string, string) {
+
         Data storage data = certData[_id];
-        
+
         return (
         data.id,
         data.sname,
-        data.course
-        ); // getting data from mapping 
+        data.course,
+        data.imghash
+        ); // getting data from mapping
       }
     function editData( string _id  , string _sname , string _course) public restricted {
-        
+
         Data storage data = certData[_id];
         data.sname = _sname;
         data.course= _course;
         // editing data in case of any error
     }
-    
+
      function totalCertificate() public view returns(uint){
          return total;
      }
---
+++ pages/autoshow/demo.js
@@ -0,0 +1,121 @@
+import React , {Component} from 'react';
+import {Button , Form , Message , Input} from 'semantic-ui-react';
+import web3 from '../../ethereum/web3';
+import factory from '../../ethereum/factory';
+import {Link} from '../../routes';
+
+
+class Demo extends Component {
+  static async getInitialProps(props) {
+    const summary = await factory.methods.getData(props.query.address).call();
+   console.log("@@@",props.query.address);
+     return{
+       address: props.query.address,
+       name:summary[1],
+       course:summary[2]
+     };
+ }
+  state = {
+   uid:'',
+	 name:'',
+	 course:'',
+	 loading : false,
+   errorMessage: '',
+   show:false
+ };
+
+
+  render(){
+
+    let person = null;
+    if(true)
+    {
+      person = (
+        <div>
+        <style jsx>{
+          `
+
+          .a11{
+            width:800px;
+            height:600px;
+            padding:20px;
+             text-align:center;
+            /* border: 8px solid #a87207;*/
+
+             background-image: url("/my-image1.png");
+              background-color: #cccccc;
+              background-position: center;
+              background-repeat: no-repeat;
+              background-size: cover;
+
+          }
+          .a12{
+            width:750px;
+             height:550px;
+              padding:20px;
+              text-align:center;
+            /*  border: 5px solid #a87207; */
+          }
+
+          .a13{
+            font-size:40px;
+            font-weight:bold;
+            color: #a81c07;
+          }
+          .a25{
+            font-size:25px;
+          }
+          .a30{
+            font-size:25px;
+          }
+          .a31{
+            font-size:30px;
+            font-weight:bold;
+            color: #8b15c2;
+            text-decoration: underline;
+          }
+          img {
+            padding: 5px;
+            width: 500px;
+            height: 200;
+          }
+
+          `
+        } </style>
+        <div className= "a11">
+        <div className= "a12">
+              <img src="/my-image.png" alt="my image" />
+               <p><span className="a13">Certificate of Course Completion</span></p>
+               <p><span className="a30"><i>This is to certify that </i></span></p>
+               <span className= "a31" ><b>{this.props.name} </b></span>
+              <p> <span className= "a25" ><i>has successfully completed the 7-days hands on webinar on </i></span></p>
+               <p><span className= "a31" >{this.props.course} </span></p>
+               <span className= "a25" >having Id. {this.props.address} </span>
+        </div>
+        </div>
+        </div>
+      );
+    }
+    return(
+
+      <div className ="m2">
+      <h3>Show Certificate</h3>
+        <Form onSubmit = {this.onSubmit}>
+          <Form.Field>
+            <label>Certificate Id</label>
+            <Input
+              value = {this.state.uid}
+              onChange= {event => this.setState({uid: event.target.value})}
+              placeholder="enter UID"
+            />
+          </Form.Field>
+
+          <Button primary loading ={this.state.loading}> Search!</ Button>
+        </Form>
+        {person}
+        </div>
+    );
+  }
+}
+
+export default Demo ;
